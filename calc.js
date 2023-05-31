b9ff10662c1a97103e2ab00c09c943b2b0821cb7
Malavi10 <131436564+Malavi10@users.noreply.github.com>
2023-05-31 12:48:23 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+let display = document.querySelector('#display');
+
+function addToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
